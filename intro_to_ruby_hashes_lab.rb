c77886837a5c868f03926d8ac13fc960dc10982b
Mary Kang <mary.kang09@gmail.com>
2020-02-29 19:39:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,10 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {:railroads => {:pieces => 4, :rent_in_dollars => {key1: "", key2: "", key3: "", key4: ""}, :names =>""}}
+  {:railroads => {
+    :pieces => 4,
+    :rent_in_dollars => {key1: "", key2: "", key3: "", key4: ""},
+    :names =>{thing1:"", thing2:"", thing3:"", thing4:""}}}
 end
 
 def monopoly_with_fourth_tier
